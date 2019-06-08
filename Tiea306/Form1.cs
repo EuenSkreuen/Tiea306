41ad6574e0d395c9763a1ba6d2b8960ee96aa1b9
Aapo Alvi Aleksi Peiponen <aapo.peiponen@gmail.com>
2019-06-08 21:16:47 +0300

    Ei lisäyksiä

--- Tiea306/Form1.cs
+++ Tiea306/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Tiea306
 {
@@ -15,6 +16,11 @@ namespace Tiea306
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
